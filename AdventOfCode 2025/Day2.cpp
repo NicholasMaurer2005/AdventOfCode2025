8c9cb151fda95c91e8c374bffe03507b5e09ad6e
Salockin <salockin9@gmail.com>
2025-12-03 02:22:10 -0500

    added some things

---
+++ AdventOfCode 2025/Day2.cpp
@@ -0,0 +1,22 @@
+#include "Days.h"
+
+#include "Helper.h"
+#include "SplitString.h"
+
+#include <iostream>
+#include <algorithm>
+#include <string_view>
+#include <string>
+
+
+
+void Day2Part1() noexcept
+{
+	const std::string data{ readFile("Day2.txt") };
+	std::cout << data << std::endl;
+}
+
+void Day2Part2() noexcept
+{
+
+}
--- AdventOfCode 2025/Days.h
+++ AdventOfCode 2025/Days.h
@@ -5,3 +5,7 @@
 void Day1Part1() noexcept;
 
 void Day1Part2() noexcept;
+
+void Day2Part1() noexcept;
+
+void Day2Part2() noexcept;
